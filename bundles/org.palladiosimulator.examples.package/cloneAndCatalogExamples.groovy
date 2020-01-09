a8e9b9154710d00fef62d18fb0b960dc5e8c8e6d
Dominik Werle <dominik.werle@kit.edu>
2020-01-09 11:40:28 +0100

    cleanup with debug messages, fixed .project/.settings removal

--- bundles/org.palladiosimulator.examples.package/cloneAndCatalogExamples.groovy
+++ bundles/org.palladiosimulator.examples.package/cloneAndCatalogExamples.groovy
@@ -1,34 +1,76 @@
 import groovy.util.XmlSlurper
 
 /*
-	def examples_target = "initiatorTemplates"
-	def catalogue_target = "Examples.architecturaltemplates"
+// set by the maven build
+def examples_source = "initiatorTemplates"
+def examples_target = "initiatorTemplates"
+def catalogue_target = "Examples.architecturaltemplates"
 */
 
+// create target directory if it does not exist
+def examples_target_filehandle = new File(examples_target)
+if (!examples_target_filehandle.exists()) {
+	examples_target_filehandle.mkdirs()
+}
+
+
+// discover all folders that contain a .project file
+println "Starting example project discovery:"
 projects = []
-new File(examples_target).eachFileRecurse {
+new File(examples_source).eachFileRecurse {
 	if (it.name == ".project") {
-		projects.add(new File(it.parent))
+		parent_folder = new File(it.parent)
+		
+		println " - ${parent_folder.name}"
+		target_folder = "${examples_target}/${parent_folder.name}"
+		
+		command = ["cp", "-r", parent_folder.path, target_folder]
+		println " -- Copying: " + command.join(" ")
+		command.execute().waitFor()
+		
+		projectXml = new XmlSlurper().parse(new File("${target_folder}/.project"))
+		entityName = "${projectXml.name}"
+		println " -- Discovered project name: ${entityName}"
+		
+		documentationFile = new File("${target_folder}/.documentation")
+		if (documentationFile.exists()) {
+			documentation = documentationFile.text
+			println " -- Documentation found"
+		} else {
+			documentation = ""
+			println " -- No documentation found"
+		}
+		
+		settingsFolder = new File("${target_folder}/.settings")
+		if (settingsFolder.exists()) {
+			command = ["rm" ,"-r", settingsFolder.path]
+			println " -- Removing .settings: " + command.join(" ")
+			command.execute().waitFor()
+		}
+	
+		command = ["rm", "${target_folder}/.project"]
+		println " -- Removing .project file: " + command.join(" ")
+		command.execute().waitFor()
+		
+		command = ["rm", "${target_folder}/.documentation"]
+		println " -- Removing .documentation file: " + command.join(" ")
+		command.execute().waitFor()
+		
+		projects.add([
+			entityName: entityName,
+			documentation: documentation,
+			defaultInstanceURI: entityName
+		])
 	}
 }
-projects = projects.sort()
 
+projects.sort { a, b -> a.entityName <=> b.entityName }
 
 result = """<?xml version="1.0" encoding="UTF-8"?>
 <org.palladiosimulator.architecturaltemplates:Catalog xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:org.palladiosimulator.architecturaltemplates="http://palladiosimulator.org/ArchitecturalTemplates/1.0" id="GeneratedATCatalog" entityName="Generated AT Catalog">\n"""
 
 projects.each {
-	projectXml = new XmlSlurper().parse(new File("${it}/.project"))
-	entityName = projectXml.name
-	documentationFile = new File("${it}/.documentation")
-	documentation = documentationFile.exists()
-		? documentationFile.text
-		: ""
-		
-	assert it.path.startsWith(examples_target + "/")
-	defaultInstanceURI = it.path - (examples_target + "/")
-	
-	result += """\t<ATs id="${entityName}.ID" entityName="${entityName}" documentation="${documentation}" defaultInstanceURI="${defaultInstanceURI}/"/>\n"""
+	result += """\t<ATs id="${it.entityName}.ID" entityName="${it.entityName}" documentation="${it.documentation}" defaultInstanceURI="${it.defaultInstanceURI}/"/>\n"""
 }
 
 result += """</org.palladiosimulator.architecturaltemplates:Catalog>\n"""
